21d5393a34653482a1c8c0f8e8409270b9b72fe6
ReneRamirez-Test <130073985+ReneRamirez-Test@users.noreply.github.com>
2025-03-12 00:48:02 +0000

    Rene_Api_1er_avance

--- src/app.py
+++ src/app.py
@@ -1,7 +1,20 @@
 import os
 import pandas as pd
-import seaborn as sns
+import spotipy
+#import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+
+client_id=os.environ.get("CLIENT_ID")
+client_secret= os.environ.get("CLIENT_SECRET")
+
+urn = 'spotify:artist:5TeBsszZQTyqBX4eDHdtNx' #La nva escuela
+sp = spotipy.Spotify()
+
+artist = sp.artist(urn)
+print(artist)
+
+user = sp.user('plamere')
+print(user)
